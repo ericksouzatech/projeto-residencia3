eae7de1c577eb0fff0d2a0fad8af3ab1aa3f73d1
ericksouzatech <biladero528@gmail.com>
2024-05-17 15:29:16 -0300

    finalizando o firestore

--- src/config/index.js
+++ src/config/index.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+
 const firebaseConfig = {
   apiKey: "AIzaSyD8zQiX8v8xTIUVEfnGeXqBkY2UfJIAJIo",
   authDomain: "projeto-residencia-iii.firebaseapp.com",
@@ -11,7 +12,9 @@ const firebaseConfig = {
   appId: "1:84972689817:web:840ee5213fe12dad479b08"
 };
 
+
 initializeApp(firebaseConfig);
+
 
 export const auth = getAuth();
 export const db = getFirestore();
